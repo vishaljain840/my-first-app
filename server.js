93eed7c5eb994a74498e80da4bb14bdd589ab873
vishaljain840 <vishaljain840@gmail.com>
2022-06-03 20:45:58 +0530

    make it better

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ server.use(
 );
 
 server.use(router);
-server.listen(port, () => {
-    console.log(`Server is running on ${port}`);
+server.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
